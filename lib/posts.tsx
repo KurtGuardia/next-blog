650b97181e53e2141f8b66f161c643a045499b70
Kurt Guardia <kurtguardia@gmail.com>
2022-11-07 11:59:19 +0100

    refactored for TypeScript

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -23,7 +23,10 @@ export function getSortedPostsData() {
     // Combine the data with the id
     return {
       id,
-      ...matterResult.data,
+      ...(matterResult.data as {
+        date: string
+        title: string
+      }),
     }
   })
   // Sort posts by date
@@ -47,7 +50,7 @@ export function getAllPostIds() {
   })
 }
 
-export async function getPostData(id) {
+export async function getPostData(id: String) {
   const fullPath = path.join(postsDirectory, `${id}.md`)
   const fileContents = fs.readFileSync(fullPath, 'utf8')
 
@@ -64,6 +67,9 @@ export async function getPostData(id) {
   return {
     id,
     contentHtml,
-    ...matterResult.data,
+    ...(matterResult.data as {
+      date: string
+      title: string
+    }),
   }
 }
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -3,8 +3,17 @@ import Date from '../../components/date'
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import utilStyles from '../../styles/utils.module.css'
+import { GetStaticProps, GetStaticPaths } from 'next'
 
-export default function Post({ postData }) {
+export default function Post({
+  postData,
+}: {
+  postData: {
+    title: string
+    date: string
+    contentHtml: string
+  }
+}) {
   return (
     <Layout>
       <Head>
@@ -26,7 +35,7 @@ export default function Post({ postData }) {
     </Layout>
   )
 }
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllPostIds()
   return {
     paths,
@@ -34,8 +43,10 @@ export async function getStaticPaths() {
   }
 }
 
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id)
+export const getStaticProps: GetStaticProps = async ({
+  params,
+}) => {
+  const postData = await getPostData(params?.id as string)
   return {
     props: {
       postData,
